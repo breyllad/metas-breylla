d036b9fbce7c8211b5c86c56a00a6a37f94d96df
breyllad <105427091+breyllad@users.noreply.github.com>
2024-03-21 08:19:20 -0300

    ok

--- main.js
+++ main.js
@@ -8,4 +8,17 @@ for(let i=0; i<botoes.length; i++){
         }
     botoes [i].classList.add("ativo");
     }
+const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll(."aba-conteudo")
+
+for(let i=0; i <botoes.lenght; i++){
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove ("ativo");
+            texto[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        testo[i].classList.add("ativo");
+    }
+}
 
