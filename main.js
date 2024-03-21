6ceba1cf6c4fa87fd26be80ecd2a697b11faa48b
breyllad <105427091+breyllad@users.noreply.github.com>
2024-03-21 08:25:07 -0300

    ok

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ for(let i=0; i<botoes.length; i++){
     botoes [i].classList.add("ativo");
     }
 const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo")
+const texto = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll("contador");
 
 for(let i=0; i <botoes.lenght; i++){
     botoes[i].onclick = function(){
@@ -21,4 +22,5 @@ for(let i=0; i <botoes.lenght; i++){
         testo[i].classList.add("ativo");
     }
 }
+contadores[0].textContent = "contagem regressiva";
 
