9bde7af13a27643720157522d4018ed850169281
breyllad <105427091+breyllad@users.noreply.github.com>
2024-03-21 08:45:11 -0300

    OK

--- main.js
+++ main.js
@@ -13,5 +13,5 @@ for(let i=0; i <botoes.lenght; i++){
     }
 }
 const tempoObjetivo1= new Date ("2025T00:00:00");
+let tempoAtual = new Date ();
 contadores[0].textContent = "contagem regressiva";
-
