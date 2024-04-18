1e1302c492d35c674a8d8687fc3114e34662d57e
breyllad <105427091+breyllad@users.noreply.github.com>
2024-04-18 08:37:29 -0300

    ok

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const texto = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0; i <botoes.lenght; i++){
+for(let i=0; i <botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove ("ativo");
